ae08db67825b8020f794a5d57606b7ef1879e964
Chloe <k.chloe208@gmail.com>
2019-03-13 00:19:45 -0400

    Initial Commit

---
+++ guessWhat_T12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessWhat_T12
+//
+//  Created by Chloe Kim on 2019-03-13.
+//  Copyright © 2019 T12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ guessWhat_T12Tests/guessWhat_T12Tests.swift
@@ -0,0 +1,34 @@
+//
+//  guessWhat_T12Tests.swift
+//  guessWhat_T12Tests
+//
+//  Created by Chloe Kim on 2019-03-13.
+//  Copyright © 2019 T12. All rights reserved.
+//
+
+import XCTest
+@testable import guessWhat_T12
+
+class guessWhat_T12Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
